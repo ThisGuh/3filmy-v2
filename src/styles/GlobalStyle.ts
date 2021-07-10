c3d62d2c0bcdc6f062d9fb63a9aa285f0faff196
thisGuh <jakiepolska1111@gmail.com>
2021-07-10 11:19:12 +0200

    config: update theme, lint-staged config and install react-icons

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -1,6 +1,8 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
+    @import url("https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap");
+
     html {
         font-size: ${({ theme }) => theme.fontSizes.xs};
     }
