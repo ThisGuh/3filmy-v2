0b41c82a297bc9f0d239f93e597215bb4811ed8a
thisGuh <jakiepolska1111@gmail.com>
2021-07-10 11:21:55 +0200

    config: update storybook config

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,12 @@
+const path = require('path')
+
 module.exports = {
+  webpackFinal: async (config, { configType }) => {
+    config.resolve.modules.push(path.resolve(__dirname, '../src'));
+
+    return config;
+  },
+
   stories: [
     '../src/stories/**/*.stories.mdx',
     '../src/stories/**/*.stories.@(js|jsx|ts|tsx)',
